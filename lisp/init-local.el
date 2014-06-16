2f5b46b33335b22b09fcbcd6acfca15b1f38be80
Josiah Knoll <josiahknoll@gmail.com>
2014-06-16 14:19:11 -0400

    fixed issues with ansi-term output

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,7 +1,8 @@
 (require 'init-sr-speedbar)
 (require 'init-evil)
 (require 'init-saves)
-(require 'init-graphene-small)
+;(require 'init-graphene-small)
+(require 'init-helper-functions)
 (require 'init-keys)
 
 (provide 'init-local)
