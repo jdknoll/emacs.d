f7a7e82171aa4aa5726cea91998f9cb7a853e3e5
Josiah Knoll <josiahknoll@gmail.com>
2014-06-18 14:47:08 -0400

    removed graphene from init-local.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,7 +1,6 @@
 (require 'init-sr-speedbar)
 (require 'init-evil)
 (require 'init-saves)
-;(require 'init-graphene-small)
 (require 'init-helper-functions)
 (require 'init-keys)
 (require 'init-smartparens)
