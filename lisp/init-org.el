0b643841abec4441ec9278c12596c337670085f6
Josiah Knoll <josiahknoll@gmail.com>
2014-07-11 11:40:09 -0400

    Added org-capture and org-iswitchb keyboard shortcuts

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -8,7 +8,9 @@
 
 
 (define-key global-map (kbd "C-c l") 'org-store-link)
+(define-key global-map (kbd "C-c c") 'org-capture)
 (define-key global-map (kbd "C-c a") 'org-agenda)
+(define-key global-map (kbd "C-c b") 'org-iswitchb)
 
 ;; Various preferences
 (setq org-log-done t
