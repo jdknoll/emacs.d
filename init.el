806f9e3af65bfcfc65c2833e369324e2c7990411
Josiah Knoll <josiahknoll@gmail.com>
2014-07-16 15:05:33 -0400

    re-added emacs sessions, but removed graphical window selection

--- init.el
+++ init.el
@@ -46,8 +46,8 @@
 (require 'init-ido)
 (require 'init-hippie-expand)
 (require 'init-auto-complete)
-;(require 'init-windows)
-;(require 'init-sessions)
+(require 'init-windows)
+(require 'init-sessions)
 (require 'init-fonts)
 (require 'init-mmm)
 
